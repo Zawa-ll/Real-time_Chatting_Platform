7e1ed18bb71f495e2939905f213d578aa74b1b47
Zawa_ll <zawalee33@gmail.com>
2023-07-28 16:30:00 -0700

    Bubble style added

--- src/MessageList.js
+++ src/MessageList.js
@@ -1,31 +1,59 @@
 import React from "react";
 import moment from "moment";
 
-
 const Message = ({ message }) => {
+    const bubbleStyle = {
+        display: "inline-block",
+        backgroundColor: "#007bff",
+        color: "#fff",
+        padding: "8px 12px",
+        borderRadius: "20px",
+        marginBottom: "10px",
+        maxWidth: "70%",
+    };
+
+    const authorStyle = {
+        fontSize: "14px",
+        color: "#666",
+        textAlign: "right",
+    };
+
     return (
-        <li>
-            <div className='columns'>
-                <p className='column is-three-quarters'>{message.text}</p>
-                <div className='column'>
-                    <p className='is-light'>
-                        {message.email} @ {moment(message.created).format('h:mm a')}
+        <li style={{ textAlign: message.email === "current_user_email" ? "right" : "left" }}>
+            <div className="columns">
+                <div className="column is-three-quarters">
+                    <div style={bubbleStyle}>{message.text}</div>
+                </div>
+                <div className="column">
+                    <p className="is-light" style={authorStyle}>
+                        {message.email} @ {moment(message.created).format("h:mm a")}
                     </p>
                 </div>
             </div>
         </li>
     );
-}
+};
 
 const MessageList = ({ messages }) => {
+    const messageListStyle = {
+        height: "80vh",
+        listStyleType: "none",
+        padding: "0",
+        margin: "0",
+        overflowY: "auto",
+    };
+
     return (
-        <ul style={{ height: '80vh' }}>
+        <ul style={messageListStyle}>
             {Object.keys(messages)
-                .map(messageKey => ({ ...messages[messageKey], id: messageKey }))
-                .map(message => <Message key={message.id} message={message}>{message.text}</Message>)
-            }
+                .map((messageKey) => ({ ...messages[messageKey], id: messageKey }))
+                .map((message) => (
+                    <Message key={message.id} message={message}>
+                        {message.text}
+                    </Message>
+                ))}
         </ul>
-    )
-}
+    );
+};
 
 export default MessageList;
